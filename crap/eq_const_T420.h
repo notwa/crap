db5933978a7ac6cda3ebdeb011be0e9b68c029f3
Connor Olding <cloningdonor@gmail.com>
2015-05-06 13:32:45 -0700

    take two

--- crap/eq_const_T420.h
+++ crap/eq_const_T420.h
@@ -55,18 +55,16 @@ INNER void
 adjust(personal *data, ulong fs)
 {
 	biquad *filters = data->filters[0];
-	// TODO: reduce ~3dB peaks at 1680, 2200
-	// TODO: reduce small peak at 600, 3780, 10600
-	filters[ 0] = biquad_gen(FILT_PEAKING,    150.,  12., 1.50, fs);
-	filters[ 1] = biquad_gen(FILT_PEAKING,    640.,  7.0, 0.80, fs);
+	filters[ 0] = biquad_gen(FILT_PEAKING,    180.,  11., 1.40, fs);
+	filters[ 1] = biquad_gen(FILT_PEAKING,    740.,  5.5, 0.70, fs);
 	filters[ 2] = biquad_gen(FILT_PEAKING,    1220, -12., 0.70, fs);
-	filters[ 3] = biquad_gen(FILT_PEAKING,    1670,  9.0, 0.18, fs);
-	filters[ 4] = biquad_gen(FILT_PEAKING,    2270,  9.0, 0.20, fs);
-	filters[ 5] = biquad_gen(FILT_PEAKING,    2470, -7.0, 0.16, fs);
-	filters[ 6] = biquad_gen(FILT_PEAKING,    3200, -0.1, 0.25, fs);
-	filters[ 7] = biquad_gen(FILT_PEAKING,    6200, -3.5, 0.25, fs);
-	filters[ 8] = biquad_gen(FILT_PEAKING,    6000, -9.0, 4.00, fs);
-	filters[ 9] = biquad_gen(FILT_PEAKING,   11560,  6.0, 0.12, fs);
-	filters[10] = biquad_gen(FILT_HIGHSHELF, 11000,  5.0, 0.50, fs);
-	filters[11] = biquad_gen(FILT_HIGHPASS,    120,  0.0, 1.00, fs);
+	filters[ 3] = biquad_gen(FILT_PEAKING,    1670,  6.0, 0.18, fs);
+	filters[ 4] = biquad_gen(FILT_PEAKING,    2080, -2.5, 0.30, fs);
+	filters[ 5] = biquad_gen(FILT_PEAKING,    2270,  6.0, 0.20, fs);
+	filters[ 6] = biquad_gen(FILT_PEAKING,    2470, -7.0, 0.16, fs);
+	filters[ 7] = biquad_gen(FILT_PEAKING,    3780, -1.5, 0.38, fs);
+	filters[ 8] = biquad_gen(FILT_PEAKING,    6200, -3.5, 0.25, fs);
+	filters[ 9] = biquad_gen(FILT_PEAKING,    6000, -9.0, 4.00, fs);
+	filters[10] = biquad_gen(FILT_HIGHSHELF, 12000,  4.0, 0.40, fs);
+	filters[11] = biquad_gen(FILT_HIGHPASS,    150,  0.0, 1.00, fs);
 }
