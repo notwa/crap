ffa6c66a100a7560435c115d18b866c9eddeba79
Connor Olding <cloningdonor@gmail.com>
2015-05-08 14:57:19 -0700

    take three

--- crap/eq_const_T420.h
+++ crap/eq_const_T420.h
@@ -11,7 +11,7 @@
 
 #include "util.h"
 
-#define BANDS 12
+#define BANDS 16
 typedef struct {
 	biquad filters[2][BANDS];
 } personal;
@@ -58,13 +58,17 @@ adjust(personal *data, ulong fs)
 	filters[ 0] = biquad_gen(FILT_PEAKING,    180.,  11., 1.40, fs);
 	filters[ 1] = biquad_gen(FILT_PEAKING,    740.,  5.5, 0.70, fs);
 	filters[ 2] = biquad_gen(FILT_PEAKING,    1220, -12., 0.70, fs);
-	filters[ 3] = biquad_gen(FILT_PEAKING,    1670,  6.0, 0.18, fs);
+	filters[ 3] = biquad_gen(FILT_PEAKING,    1580,  7.0, 0.25, fs);
 	filters[ 4] = biquad_gen(FILT_PEAKING,    2080, -2.5, 0.30, fs);
 	filters[ 5] = biquad_gen(FILT_PEAKING,    2270,  6.0, 0.20, fs);
-	filters[ 6] = biquad_gen(FILT_PEAKING,    2470, -7.0, 0.16, fs);
-	filters[ 7] = biquad_gen(FILT_PEAKING,    3780, -1.5, 0.38, fs);
+	filters[ 6] = biquad_gen(FILT_PEAKING,    2470, -2.0, 0.18, fs);
+	filters[ 7] = biquad_gen(FILT_PEAKING,    3700, -5.0, 0.32, fs);
 	filters[ 8] = biquad_gen(FILT_PEAKING,    6200, -3.5, 0.25, fs);
-	filters[ 9] = biquad_gen(FILT_PEAKING,    6000, -9.0, 4.00, fs);
-	filters[10] = biquad_gen(FILT_HIGHSHELF, 12000,  4.0, 0.40, fs);
+	filters[ 9] = biquad_gen(FILT_PEAKING,    6000, -11., 3.66, fs);
+	filters[10] = biquad_gen(FILT_HIGHSHELF, 11500,  4.0, 0.40, fs);
 	filters[11] = biquad_gen(FILT_HIGHPASS,    150,  0.0, 1.00, fs);
+	filters[12] = biquad_gen(FILT_PEAKING,    1775, -2.0, 0.18, fs);
+	filters[13] = biquad_gen(FILT_PEAKING,     490, -1.5, 0.23, fs);
+	filters[14] = biquad_gen(FILT_PEAKING,    3100,  5.0, 0.33, fs);
+	filters[15] = biquad_gen(FILT_LOWPASS,   14000,  0.0, 0.40, fs);
 }
